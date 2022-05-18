ec0c8403b9bb3fa95696a97c1a5441910af4fde0
Chris-Durr <chrisdurr88@gmail.com>
2022-05-18 12:16:43 +0000

    2wops

--- src/models.py
+++ src/models.py
@@ -20,29 +20,39 @@ class Favoritos_Pl(Base):
     id = Column(Integer, primary_key=True)
     id_user = Column(Integer)
     id_pl = Column(Integer)
+    user = relationship(User)
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    population = Column(Integer)
+    gravity =  Column(String(250), nullable=False)
 
 class Favoritos_Pj(Base):
     __tablename__ = 'favoritos_pj'
     id = Column(Integer, primary_key=True)
     id_user = Column(Integer)
     id_pj = Column(Integer)
+    user = relationship(User)
 
 class Personajes(Base):
     __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
-    nombre = Column(Integer)
-    id_pj = Column(Integer)
+    nombre = Column(String(250), nullable=False)
+    estatura = Column(Integer)
+    colorOjos =  Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
     def to_dict(self):
         return {}
